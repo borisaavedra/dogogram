3d3dacf45654d3912e911d96be6223e9110ada5d
Boris Saavedra <boris.saavedra@gmail.com>
2021-02-25 20:33:24 -0400

    ALLOWED_HOST typo

--- dogogram/settings.py
+++ dogogram/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["https://dogogramoficial.herokuapp.com/"]
+ALLOWED_HOSTS = ["dogogramoficial.herokuapp.com"]
 
 
 # Application definition
