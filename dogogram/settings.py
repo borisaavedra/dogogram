6d6e0982d33abf49b12c0bb249bd0bbd3453dcc3
Boris Saavedra <boris.saavedra@gmail.com>
2021-02-26 21:37:02 -0400

    Debug False

--- dogogram/settings.py
+++ dogogram/settings.py
@@ -30,7 +30,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ["dogogramoficial.herokuapp.com"]
 
